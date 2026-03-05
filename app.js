e94d0f109fe0595948d3e12a66dc86e11fb272a0
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-05 21:21:54 +0500

    Initial commi

---
+++ app.js
@@ -0,0 +1,5 @@
+// 1. Match first letter of first element in array with first letter of each element in array
+// 2. If match, save this letter and then check if the next letter of first element in array matches with next letter of each element in array
+
+// var longestCommonPrefix = function (strs) {
+//     if (strs.length === 1) return strs[0][0];
