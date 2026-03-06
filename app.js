78fdb50bd4a70991118609d79b829703b64be29c
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-06 12:58:34 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // 1. Match first letter of first element in array with first letter of each element in array
 // 2. If match, save this letter and then check if the next letter of first element in array matches with next letter of each element in array
 
-var longestCommonPrefix = function (strs) {
+// var longestCommonPrefix = function (strs) {
 
-    if (strs.length === 1) return strs[0];
+//     if (strs.length === 1) return strs[0];
 
-    let prefix = "";
-    let currentLetter = "";
+//     let prefix = "";
+//     let currentLetter = "";
 
-    for (let i = 0; i < strs[0].length; i++) {
+//     for (let i = 0; i < strs[0].length; i++) {
 
-        currentLetter = strs[0][i];
+//         currentLetter = strs[0][i];
 
-        for (let j = 1; j < strs.length; j++) {
+//         for (let j = 1; j < strs.length; j++) {
 
-            if (currentLetter !== strs[j][i]) {
-                return prefix;
-            }
-        }
+//             if (currentLetter !== strs[j][i]) {
+//                 return prefix;
+//             }
+//         }
 
-        prefix += currentLetter;
-    }
+//         prefix += currentLetter;
+//     }
 
-    return prefix;
-};
+//     return prefix;
+// };
 
-const arr = [""];
+// const arr = [""];
 
-let longestCommonPrefixVar = longestCommonPrefix(arr);
+// let longestCommonPrefixVar = longestCommonPrefix(arr);
 
-console.log("longestCommonPrefixVar: ", longestCommonPrefixVar);
+// console.log("longestCommonPrefixVar: ", longestCommonPrefixVar);
