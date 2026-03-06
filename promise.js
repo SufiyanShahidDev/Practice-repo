45e301bb987bbac065c8820bff2a0560c9045bb7
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-06 22:35:00 +0500

    Initial commit

--- promise.js
+++ promise.js
@@ -1,31 +1,31 @@
 
-const fetchData = new Promise((resolve, reject) => {
+// const fetchData = new Promise((resolve, reject) => {
 
-    setTimeout(() => {
-        resolve("Data received from server");
-    }, 2000);
+//     setTimeout(() => {
+//         resolve("Data received from server");
+//     }, 2000);
 
-});
+// });
 
-fetchData
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+// fetchData
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
 
-// login Promise
+// // login Promise
 
-const login = new Promise((resolve, reject) => {
-    let password = 1234;
+// const login = new Promise((resolve, reject) => {
+//     let password = 1234;
 
-    if(password === 123){
-        resolve("Login Successfully")
-    }
-    else{
-        reject("Wrong Password")
-    }
-}
+//     if(password === 123){
+//         resolve("Login Successfully")
+//     }
+//     else{
+//         reject("Wrong Password")
+//     }
+// }
 
-)
+// )
 
-login
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+// login
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
--- revision.js
+++ revision.js
@@ -4,3 +4,9 @@
 array.map((element, index, array) => {
    return newValue;
 })
+
+let prices = [100, 200, 300];
+
+let pricesWithTax = prices.map(price => price * 1.1);
+
+console.log(pricesWithTax);
