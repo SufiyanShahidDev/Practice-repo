504724f6d34f8b5ccf390690cc67d1dafd30de18
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-06 12:34:01 +0500

    Initial commit

--- app.js
+++ app.js
@@ -2,20 +2,17 @@
 // 2. If match, save this letter and then check if the next letter of first element in array matches with next letter of each element in array
 
 var longestCommonPrefix = function (strs) {
-    if (strs.length === 1) return strs[0][0];
+
+    // if (strs.length === 1) return strs[0];
 
     let prefix = "";
     let currentLetter = "";
 
     for (let i = 0; i < strs[0].length; i++) {
-        // i = 1
-        // currentLetter = e
-        if (strs[0][i] == undefined) return prefix;
 
         currentLetter = strs[0][i];
+
         for (let j = 1; j < strs.length; j++) {
-            // j = 1
-            // h                strs[1][1] = h
 
             if (currentLetter !== strs[j][i]) {
                 return prefix;
@@ -28,7 +25,6 @@ var longestCommonPrefix = function (strs) {
     return prefix;
 };
 
-const arr = ["flower", "flower", "flower", "flower"];
 
 let longestCommonPrefixVar = longestCommonPrefix(arr);
 
