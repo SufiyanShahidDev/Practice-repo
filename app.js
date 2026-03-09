c9a490bf8feb7a175f4ee9028fef9b35c73270c9
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-09 22:49:49 +0500

    Initial commit

--- app.js
+++ app.js
@@ -51,3 +51,8 @@ setTimeout(() => {
   console.log("Timeout 1");
   Promise.resolve().then(() => console.log("Promise inside Timeout"));
 }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log("Promise 1");
+//   setTimeout(() => console.log("Timeout inside Promise"), 0);
+// });
